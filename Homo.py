12eb88aeb84e98803d18445989e36444224b9b43
HayatoKarasu <138600849+HayatoKarasu@users.noreply.github.com>
2023-07-04 21:46:32 +0700

    Add files via upload

---
+++ Homo.py
@@ -0,0 +1,8 @@
+﻿print("Тест: Происхождение человека")
+a = input("Введите первую стадию: ") #“homo habilis”
+b = input("Введите вторую стадию: ") #“homo erectus”
+c = input("Введите третью стадию: ") #“homo heidelbergensis”
+d = input("Введите четвертую стадию: ") #“Homo neanderthalensis”
+e = input("Введите пятую стадию: ") #“homo sapiens”
+f = input("Введите последнюю стадию: ") #“homo sapiens sapiens”
+print(a, b, c, d, e, f, sep = "=>")
